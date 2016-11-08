07c261ca9232a6ff54f1b0b3d1721054ea43fe0a
brian <brianmatheus@gmail.com>
2016-11-07 19:13:17 -0600

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+let gulp          = require('gulp');
+let browserSync   = require('browser-sync');
+let sass          = require('gulp-sass');
+let plumber       = require('gulp-plumber');
+let autoprefixer  = require('gulp-autoprefixer');
+let concat        = require('gulp-concat');
+let uglify        = require('gulp-uglify');
+
+gulp.task('serve', ['watch'], () => {
+  browserSync.init({
+     server: {
+         baseDir: './'
+     }
+  });
+});
+
+gulp.task('watch', () => {
+  gulp.watch('./css/scss/*.scss', ['sass']);
+  gulp.watch('./js/files/*.js', ['js']);
+});
+
+gulp.task('sass', () => {
+    return gulp.src('./css/scss/styles.scss')
+      .pipe(plumber())
+      .pipe(sass())
+      .pipe(gulp.dest('./css'))
+      .pipe(browserSync.stream());
+});
+
+gulp.task('js', () => {
+  return gulp.src('./js/files/*.js')
+    .pipe(uglify())
+    .pipe(concat('scripts.min.js'))
+    .pipe(gulp.dest('./js'))
+    .pipe(browserSync.reload());
+});
+
+gulp.task('default', ['serve']);
